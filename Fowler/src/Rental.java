1e9c5be7fcdfead4abc8ecd6713b61e4a77073aa
Oliver Saar <oli.saar123@gmail.com>
2023-05-10 18:53:45 +0200

    Initial commit

---
+++ Fowler/src/Rental.java
@@ -0,0 +1,17 @@
+class Rental {
+    private final Movie movie;
+    private final int daysRented;
+
+    public Rental(Movie newmovie, int newdaysRented) {
+        movie = newmovie;
+        daysRented = newdaysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+}
